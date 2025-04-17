55b59cb747718161a9ab38d91c7c0b5a7578070c
BugsBunny011 <parvbunny@gmail.com>
2025-04-17 13:34:01 +0530

    Second commit

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -30,7 +30,7 @@ function CardItem(props) {
           offset={-80}
         >
           <figure className='cards__item__pic-wrap' data-category={props.label}>
-            <img src={props.src} alt='Travel Image' className='cards__item__img' />
+            <img src={props.src} alt='Travel' className='cards__item__img' />
           </figure>
           <div className='cards__item__info'>
             <h5 className='cards__item__text'>{props.text}</h5>
